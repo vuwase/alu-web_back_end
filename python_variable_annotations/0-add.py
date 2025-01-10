2da809ee1304057971c6ab6ca0f639e521e624b6
vuwase <v.uwase@alustudent.com>
2025-01-10 20:33:43 +0200

    done

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
-
-"""
-define a type annotated function that adds two float
-numbers and return the sum as a float
+""" 0. Basic annotations - add
 """
 
 
 def add(a: float, b: float) -> float:
-    """
-    Add two float numbers and return the sum as a float
+    """ Adds two float number numbers together.
     """
     return a + b
-
-
-if __name__ == "__main__":
-    print(add(10.5, 20.5))  # 31.0
-    print(add(10.0, 20))  # 30.0
